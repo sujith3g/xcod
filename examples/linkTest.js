48d9802f49d3c1c4d97fd76effd29bed2ab4dff7
Sujith G <sujith3g@gmail.com>
2016-01-10 01:32:56 +0530

    testing with intelligentTagging

--- examples/linkTest.js
+++ examples/linkTest.js
@@ -1,10 +1,17 @@
 DocxGen=require('../js/docxgen.js')
 var fs = require('fs');
 var docx = new DocxGen().loadFromFile('linkExample1.docx',{
+  "intelligentTagging":true,
   "replaceLinks": {
     "Nulla":"http://nulla.com",
     "Nunc":"http://nunc.com"
   }
+});
+docx.setTags({
+  "links":[
+  {"link":"Nulla"},
+  {"link":"Nunc"}
+  ]
 });
     docx.finishedCallback = function() {
       out = docx.output({
